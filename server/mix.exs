d9b0be76b0ee32b625180f968bf1583a2dcb1754
Kanmii <maneptha@gmail.com>
2018-05-25 10:25:31 +0200

    Compile factory.ex only in dev.

--- server/mix.exs
+++ server/mix.exs
@@ -25,7 +25,8 @@ defmodule Gas.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "dev"]
+  defp elixirc_paths(:test), do: elixirc_paths(:dev) ++ ["test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
