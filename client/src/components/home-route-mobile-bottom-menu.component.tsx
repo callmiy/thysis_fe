606314e3539a1d6a5fbb2a4bea7296aa9a1206e1
Kanmii <maneptha@gmail.com>
2018-06-03 04:35:13 +0200

    Create new tag - rendered as modal. No server communication yet.

--- client/src/components/home-route-mobile-bottom-menu.component.tsx
+++ client/src/components/home-route-mobile-bottom-menu.component.tsx
@@ -1,35 +1,149 @@
 import React from "react";
-import { Menu, Icon } from "semantic-ui-react";
+import {
+  Menu,
+  Icon,
+  Button,
+  Modal,
+  Form,
+  Input,
+  Header
+} from "semantic-ui-react";
 
-export default class NewQuoteMobileBottomMenu extends React.PureComponent {
+// import { SimpleCss } from "../constants";
+
+const styles = {
+  container: {
+    flexShrink: 0, // don't allow flexbox to shrink it
+    borderRadius: 0, // clear semantic-ui style
+    margin: 0 // clear semantic-ui style
+  },
+
+  newTagModalForm: {
+    marginTop: "50%"
+  }
+}; // as SimpleCss;
+
+interface NewTagModalFormProps {
+  open: boolean;
+  dismissModal: () => void;
+}
+
+interface NewTagModalFormState {
+  text: string;
+}
+
+const initalStateNewTagModalFormState: NewTagModalFormState = {
+  text: ""
+};
+
+class NewTagModalForm extends React.PureComponent<
+  NewTagModalFormProps,
+  NewTagModalFormState
+> {
+  state = initalStateNewTagModalFormState;
+
+  constructor(props: NewTagModalFormProps) {
+    super(props);
+
+    ["handleChange"].forEach(fn => (this[fn] = this[fn].bind(this)));
+  }
+
   render() {
+    const { open } = this.props;
+
     return (
-      <Menu
-        pointing={true}
-        icon="labeled"
-        borderless={true}
-        widths={3}
-        style={{
-          flexShrink: 0, // don't allow flexbox to shrink it
-          borderRadius: 0, // clear semantic-ui style
-          margin: 0 // clear semantic-ui style
-        }}
+      <Modal
+        style={styles.newTagModalForm}
+        basic={true}
+        size="small"
+        open={open}
       >
-        <Menu.Item>
-          <Icon name="tag" />
-          New Tag
-        </Menu.Item>
+        <Header icon="quote left" content="Subject matter of quote" />
 
-        <Menu.Item>
-          <Icon name="user" />
-          New Source
-        </Menu.Item>
+        <Modal.Content>
+          <Form.Field
+            control={Input}
+            placeholder="Tag text"
+            fluid={true}
+            onChange={this.handleChange}
+          />
+        </Modal.Content>
 
-        <Menu.Item>
-          <Icon name="numbered list" />
-          Tags
-        </Menu.Item>
-      </Menu>
+        <Modal.Actions>
+          <Button basic={true} color="red" inverted={true} onClick={this.reset}>
+            <Icon name="remove" /> Dismiss
+          </Button>
+          <Button color="green" inverted={true} disabled={!!!this.state.text}>
+            <Icon name="checkmark" /> Ok
+          </Button>
+        </Modal.Actions>
+      </Modal>
     );
   }
+
+  reset = () => {
+    this.props.dismissModal();
+    this.setState(initalStateNewTagModalFormState);
+  };
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = e;
+    this.setState(s => ({ ...s, text: target.value }));
+  };
+}
+
+interface NewQuoteMobileBottomMenuState {
+  newTagOpened: boolean;
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export default class NewQuoteMobileBottomMenu extends React.Component<
+  {},
+  NewQuoteMobileBottomMenuState
+> {
+  state: NewQuoteMobileBottomMenuState = {
+    newTagOpened: false
+  };
+
+  constructor(props: {}) {
+    super(props);
+
+    ["toggleNewTagModal"].forEach(fn => (this[fn] = this[fn].bind(this)));
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <NewTagModalForm
+          open={this.state.newTagOpened}
+          dismissModal={this.toggleNewTagModal(false)}
+        />
+        <Menu
+          pointing={true}
+          icon="labeled"
+          borderless={true}
+          widths={3}
+          style={styles.container}
+        >
+          <Menu.Item onClick={this.toggleNewTagModal(true)}>
+            <Icon name="tag" />
+            New Tag
+          </Menu.Item>
+
+          <Menu.Item>
+            <Icon name="user" />
+            New Source
+          </Menu.Item>
+
+          <Menu.Item>
+            <Icon name="numbered list" />
+            Tags
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+
+  toggleNewTagModal = (value: boolean) => () =>
+    this.setState(s => ({ ...s, newTagOpened: value }));
 }
