72c6bf43ed1b755815cac48a81ae466c13650a40
Kanmii <maneptha@gmail.com>
2018-10-11 17:00:28 +0200

    Using rimraf to remove webclient folder on server.

--- client/sync-backend.js
+++ client/sync-backend.js
@@ -1,27 +1,10 @@
 const path = require("path");
 const copydir = require("copy-dir");
-const fs = require("fs");
-
-const rmDir = function(dirPath) {
-  try {
-    var files = fs.readdirSync(dirPath);
-  } catch (e) {
-    return;
-  }
-  if (files.length > 0) {
-    for (var i = 0; i < files.length; i++) {
-      var filePath = dirPath + "/" + files[i];
-      if (fs.statSync(filePath).isFile()) {
-        fs.unlinkSync(filePath);
-      } else {
-        rmDir(filePath);
-      }
-    }
-  }
-  fs.rmdirSync(dirPath);
-};
 
 const buildDir = path.resolve(".", "build");
 const deployDir = path.resolve("..", "server", "priv", "web-client");
-rmDir(path.resolve(deployDir, "static"));
+
+// tslint:disable-next-line:no-console
+console.log("Copying from: ", buildDir, " to: ", deployDir);
+
 copydir.sync(buildDir, deployDir);
