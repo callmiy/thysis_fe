5f910a37e81f8902eb6459b8445bbe8ebcc29059
Kanmii <maneptha@gmail.com>
2018-11-06 17:51:00 +0100

    .iex.exs prod.

--- server/.iex.exs
+++ server/.iex.exs
@@ -14,13 +14,6 @@ alias Thises.QuoteTagApi
 alias ThisesWeb.Schema
 alias Thises.Author
 alias Thises.AuthorApi
-alias ThisesWeb.Query.Quote, as: QuoteQuery
-alias ThisesWeb.Query.Author, as: AuthorQuery
-alias ThisesWeb.Query.Source, as: SourceQuery
-alias ThisesWeb.Query.SourceType, as: SourceTypeQuery
-alias ThisesWeb.Query.Tag, as: TagQuery
-alias Thises.Query.Registration, as: RegistrationQuery
-alias ThisesWeb.Query.User, as: UserQuery
 alias ThisesWeb.Auth.Guardian, as: GuardianApp
 alias Thises.Accounts
 alias Thises.Accounts.CredentialApi
@@ -30,13 +23,3 @@ alias Thises.Accounts.UserApi
 alias Thises.Accounts.User
 alias Thises.Projects
 alias Thises.Projects.Project
-alias Thises.Factory
-alias Thises.Factory.Author, as: AuthorFactory
-alias Thises.Factory.User, as: UserFactory
-alias Thises.Factory.Source, as: SourceFactory
-alias Thises.Factory.Registration, as: RegFactory
-alias Thises.Factory.Project, as: ProjectFactory
-alias Thises.Factory.SourceType, as: SourceTypeFactory
-alias Thises.Factory.Tag, as: TagFactory
-alias Thises.Factory.Quote, as: QuoteFactory
-alias Thises.Factory.QuoteTag, as: QuoteTagFactory
