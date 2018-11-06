d602fb76c3b59f91b90944a04768c8a2d3b0dc5c
Kanmii <maneptha@gmail.com>
2018-11-06 19:22:06 +0100

    Removed projects link from header.

--- client/src/components/Header/component.tsx
+++ client/src/components/Header/component.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
-import { NavLink } from "react-router-dom";
 import { Icon } from "semantic-ui-react";
 
 import "./header.css";
 import { Props, State, INITIAL_STATE } from "./header.utils";
-import { LOGIN_URL, USER_REG_URL, PROJECTS_URL } from "../../routes/util";
 import {
   AppSidebarContext,
   SideBarContextProps
 } from "../../containers/App/app.utils";
-
-const NO_DISPLAY_PROJECT_PATHS = [LOGIN_URL, USER_REG_URL, PROJECTS_URL];
 
 export default class Header extends React.Component<Props, State> {
   state = INITIAL_STATE;
@@ -32,31 +28,14 @@ export default class Header extends React.Component<Props, State> {
           <a className="sidebar-trigger item" onClick={context.onShowClicked}>
             <Icon name="content" />
           </a>
+
           <div className="title">{title}</div>
-        </div>
 
-        <div className="bottom">
-          {this.shouldDisplayProject() && (
-            <div>
-              <NavLink to={PROJECTS_URL} className="to-projects">
-                Projects
-              </NavLink>
-
-              <div className="project-title">
-                {currentProject && currentProject.title}
-              </div>
-            </div>
+          {currentProject && (
+            <div className="project-title">currentProject.title</div>
           )}
         </div>
       </div>
     );
-  };
-
-  private shouldDisplayProject = () => {
-    const {
-      currentProject,
-      match: { path }
-    } = this.props;
-    return currentProject && !NO_DISPLAY_PROJECT_PATHS.includes(path);
   };
 }
