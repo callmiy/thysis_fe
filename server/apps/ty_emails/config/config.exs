074f7932fd49ff063c40366d231a1aeb86e0b77d
maneptha <maneptha@gmail.com>
2019-02-28 11:48:52 +0100

    Email setting on server.

--- server/apps/ty_emails/config/config.exs
+++ server/apps/ty_emails/config/config.exs
@@ -1,5 +1,14 @@
 use Mix.Config
 
+config :ty_emails, TyEmails.DefaultImpl.Mailer,
+  adapter: Swoosh.Adapters.SMTP,
+  relay: System.get_env("RESUME_MAKR_SMTP_RELAY"),
+  username: System.get_env("RESUME_MAKR_SMTP_USER"),
+  password: System.get_env("RESUME_MAKR_SMTP_PASS"),
+  tls: :always,
+  auth: :always,
+  port: (System.get_env("RESUME_MAKR_SMTP_PORT") || "0") |> String.to_integer()
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
