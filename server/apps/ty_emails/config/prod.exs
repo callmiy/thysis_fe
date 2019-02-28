2fd5935db026de9f08885f06457ff60ee5bf13fb
maneptha <maneptha@gmail.com>
2019-02-28 11:42:28 +0100

    Server email setting changed to prod.

--- server/apps/ty_emails/config/prod.exs
+++ server/apps/ty_emails/config/prod.exs
@@ -1,1 +1,10 @@
 use Mix.Config
+
+config :ty_emails, TyEmails.DefaultImpl.Mailer,
+  adapter: Swoosh.Adapters.SMTP,
+  relay: System.get_env("THYSIS_MAKR_SMTP_RELAY"),
+  username: System.get_env("THYSIS_MAKR_SMTP_USER"),
+  password: System.get_env("THYSIS_MAKR_SMTP_PASS"),
+  tls: :always,
+  auth: :always,
+  port: System.get_env("THYSIS_MAKR_SMTP_PORT") |> String.to_integer()
