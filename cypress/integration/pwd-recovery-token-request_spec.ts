db96c1be11b179b32e8fa99d93cdee489369a433
maneptha <maneptha@gmail.com>
2019-03-03 11:27:12 +0100

    Cypress - use base url

--- cypress/integration/pwd-recovery-token-request_spec.ts
+++ cypress/integration/pwd-recovery-token-request_spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 describe("Password Recovery Token Request", function() {
-  beforeEach(() => cy.visit("http://localhost:3017"));
+  beforeEach(() => cy.visit("/"));
 
   it(".should() - assert that <title> is correct", function() {
     cy.getByText(/Forgot your password/i).click();
