c46a1932545f857a52999ce5cfb099a8e8f6e9c0
maneptha <maneptha@gmail.com>
2019-03-04 02:48:26 +0100

    Adding cypress - wip

--- e2e/cypress/integration/pwd-recovery-token-request_spec.ts
+++ e2e/cypress/integration/pwd-recovery-token-request_spec.ts
@@ -1,10 +1,10 @@
-// import { routeTitle } from "../../src/PwdRecoveryTokenRequest/pwd-recovery-token-request";
+import { routeTitle } from "../../../src/PwdRecoveryTokenRequest/pwd-recovery-token-request";
 
 describe("Password Recovery Token Request", function() {
   beforeEach(() => cy.visit("/"));
 
   it(".should() - assert that <title> is correct", function() {
     cy.getByText(/Forgot your password/i).click();
-    cy.title().should("include", "Request Password Recovery");
+    cy.title().should("include", routeTitle);
   });
 });
